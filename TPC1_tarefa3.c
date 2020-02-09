4f48b115c4388fcfda615ca0a0fe54744e92c51f
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-02-09 20:35:32 +0000

    Update TPC1_tarefa3.c

--- TPC1_tarefa3.c
+++ TPC1_tarefa3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-	float a , b ,c , area ;
-	float areaTriangulo( float a , float b ,float c){
+
+float areaTriangulo( float a , float b ,float c){
 		int area = 0 , p= 0;
 
 		p = (a+b+c)/2;
@@ -11,6 +10,8 @@ int main(){
 		return area;
 	}
 
+int main(){
+	float a , b ,c , area ;
 	printf ("Introduza um dos lados do triângulo \n");
 	scanf ("%f" , a);
 	printf ("Introduza outro lado do triângulo \n");
@@ -25,6 +26,6 @@ int main(){
 		else if (a==c && a!=b) printf ("O triângulo introduzido é um triângulo isósceles e a area dele é %f" , area);
 		else if (b!=a && c!=a &&  b!=c) printf ("O triângulo introduzido é um triângulo escaleno e a area dele é %f" , area);
 	}
-	printf ("Os valores de lados introduzidos não estão corretos");
+	else printf ("Os valores de lados introduzidos não estão corretos");
 	return 0;
 }
