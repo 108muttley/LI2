e8680798e3405fc51f83b006f801e849e19f15ec
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-02-14 01:46:19 +0000

    Update TPC1_tarefa3.c

--- TPC1_tarefa3.c
+++ TPC1_tarefa3.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-float areaTriangulo( float a , float b ,float c){
-		int area = 0 , p= 0;
-
-		p = (a+b+c)/2;
-		area = sqrt(p * (p-a) * (p-b) * (p-c));
-		return area;
-	}
+#include <math.h>
 
 int main(){
-	float a , b ,c , area ;
-	printf ("Introduza um dos lados do triângulo \n");
-	scanf ("%f" , a);
-	printf ("Introduza outro lado do triângulo \n");
-	scanf ("%f" , b);
-	printf ("Introduza o ultimo lado do triângulo \n");
-	scanf ("%f" , c);
-	area = areaTriangulo(a,b,c); 
-	if (abs(b-c) < a < b+c) {
-		if (a==b && b==c) printf ("O triângulo introduzido é um triângulo equilátero e a area dele é %f" , area); 
-		else if (a==b && a!=c) printf ("O triângulo introduzido é um triângulo isósceles e a area dele é %f" , area);
-		else if (b==c && a!=b) printf ("O triângulo introduzido é um triângulo isósceles e a area dele é %f" , area);
-		else if (a==c && a!=b) printf ("O triângulo introduzido é um triângulo isósceles e a area dele é %f" , area);
-		else if (b!=a && c!=a &&  b!=c) printf ("O triângulo introduzido é um triângulo escaleno e a area dele é %f" , area);
-	}
-	else printf ("Os valores de lados introduzidos não estão corretos");
-	return 0;
+    float   a =0, b=0 ,c=0 , area=0 , p=0;
+    printf ("Introduza um dos lados do triangulo \n");
+    scanf ("%f" , &a);
+    printf ("Introduza outro lado do triangulo \n");
+    scanf ("%f" , &b);
+    printf ("Introduza o ultimo lado do triangulo \n");
+    scanf ("%f" , &c);
+
+    p = (a+b+c)/2;
+    area = sqrt(p * (p-a) * (p-b) * (p-c));
+    if (fabsf(b-c) < a && a < b+c) {
+        if (a==b && b==c) printf ("O triangulo introduzido e um triangulo equilatero e a area dele e %f" , area);
+        else if (a==b && a!=c) printf ("O triangulo introduzido e um triangulo isosceles e a area dele e %f" , area);
+        else if (b==c && a!=b) printf ("O triangulo introduzido e um triangulo isosceles e a area dele e %f" , area);
+        else if (a==c && a!=b) printf ("O triangulo introduzido e um triangulo isosceles e a area dele e %f" , area);
+        else if (b!=a && c!=a &&  b!=c) printf ("O triangulo introduzido e um triangulo escaleno e a area dele e %f" , area);
+    }
+    else printf ("Os valores de lados introduzidos nao estao corretos");
+    return 0;
 }
