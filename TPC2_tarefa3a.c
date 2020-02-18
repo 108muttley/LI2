b48f0d51fbe44e8c0fd4253715e498518c91113b
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-02-18 22:12:01 +0000

    Update TPC2_tarefa3a.c

--- TPC2_tarefa3a.c
+++ TPC2_tarefa3a.c
@@ -2,39 +2,27 @@
 #include <stdio.h>
 #include <math.h>
 
-int multiplicacao () {
-		int x , y , resposta;
-		x = rand() % 10;
-		y = rand() % 10;
+void multiplicacao(int t , int x , int y){
+    int resposta;
+    if (t) {
+        x = rand() % 10;
+        y = rand() % 10;
+    }
+    printf("Quanto e %d vezes %d?\n", x ,y);
+    scanf ("%d", &resposta);
 
-		printf("Quanto e %d vezes %d?",x, y);
-		scanf ("%d" , &resposta);
-		if (resposta == x*y) {
-			printf ("Muito bem!!");
-			multiplicacao();
-		}
-		else {
-			printf("Nao! Tenta outra vez.");
-			multiplicacao2(x,y);
-		}
-	}
+    if (resposta == x*y ){
+        printf ("Muito bem!!\n");
+        multiplicacao(1 , x ,y);
+    }
+    else {
+        printf("Nao! Tenta outra vez.\n");
+        multiplicacao(0 , x ,y);
+    }
 
-int multiplicacao2 (int x, int y) {
-		int resposta;
-
-		printf("Quanto e %d vezes %d?",x, y);
-		scanf ("%d" , &resposta);
-		if (resposta == x*y) {
-			printf ("Muito bem!!");
-			multiplicacao();
-		}
-		else {
-			printf("Nao! Tenta outra vez.");
-			multiplicacao2(x,y);
-		}
-	}
+}
 
 int main(){
-	multiplicacao();
-	return 0;
+    multiplicacao(1 , 0 ,0);
+    return 0;
 }
