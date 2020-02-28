28c647f0ac5af634de578a36ccd59e815b2b4f0a
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-02-28 01:29:08 +0000

    Create TPC3_desenhos.h

---
+++ TPC3_desenhos.h
@@ -0,0 +1,9 @@
+
+
+#ifndef UNTITLED6_DESENHOS_H
+#define UNTITLED6_DESENHOS_H
+
+void imprime_triangulo(int num_linhas);
+void imprime_losango(int numero);
+void imprime_hexagono(int num);
+#endif //UNTITLED6_DESENHOS_H
