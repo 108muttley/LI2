538a2ccb34a59b36e95f84f04d8b486a49fba292
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-02-28 01:30:04 +0000

    Delete TPC3_desenhosh.c

--- TPC3_desenhosh.c
+++
@@ -1,9 +0,0 @@
-
-
-#ifndef UNTITLED6_DESENHOS_H
-#define UNTITLED6_DESENHOS_H
-
-void imprime_triangulo(int num_linhas);
-void imprime_losango(int numero);
-void imprime_hexagono(int num);
-#endif //UNTITLED6_DESENHOS_H
