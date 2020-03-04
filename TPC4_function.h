dba3cdb3edce9def37701610d9ed26f79732f0ce
Bárbara Teixeira <51086765+108muttley@users.noreply.github.com>
2020-03-04 03:07:33 +0000

    Create TPC4_function.h

---
+++ TPC4_function.h
@@ -0,0 +1,6 @@
+#ifndef TPC4LI2_FUNCION_H
+#define TPC4LI2_FUNCION_H
+void soma_elemento(int *arr, int dim, int idx);
+void roda_esq(int *arr, int dim, int shifter);
+int remove_menores(int *arr, int dim, int valor);
+#endif //TPC4LI2_FUNCION_H
